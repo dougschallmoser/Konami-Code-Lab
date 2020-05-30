78c0cb253b9e80c8817b17d6ab182b929cf244c7
Douglas Schallmoser <65590878+dougschallmoser@users.noreply.github.com>
2020-05-29 22:40:17 -0700

    My Completed Code

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,25 @@ const codes = [
   "a"
 ];
 
+
+// Build a function to check if Konami Code is valid
 function init() {
-  // your code here
+  let index = 0;
+
+  document.body.addEventListener('keydown', function(e) {
+    const key = e.key;
+
+      if (key === codes[index]) {
+        index++;
+
+          if (index === codes.length) {
+            alert("Hurray");
+            index = 0;
+            }
+      } else {
+        index = 0;
+        }
+    })
 }
+
+init()
